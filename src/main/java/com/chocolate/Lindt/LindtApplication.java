91d4085652b0e4a973f46f29cb84738a428b58d4
DianaMihaela2020 <ciobanudiana91@gmail.com>
2020-12-05 12:35:52 +0200

    initial setup

---
+++ src/main/java/com/chocolate/Lindt/LindtApplication.java
@@ -0,0 +1,13 @@
+package com.chocolate.Lindt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocolate/Lindt/LindtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolate.Lindt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
