f30d847159ec853bb839a4a67f55a95bce95c12d
DianaMihaela2020 <ciobanudiana91@gmail.com>
2020-12-11 23:09:05 +0200

    Am personalizat meniul

--- src/main/java/com/chocolate/Lindt/controller/MainController.java
+++ src/main/java/com/chocolate/Lindt/controller/MainController.java
@@ -9,4 +9,45 @@ public class MainController {
     public String index() {
         return "home";
     }
+
+    @RequestMapping("/brand")
+    public String brand() {
+        return "brand";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping("/formular_ciocolata")
+    public String formular_ciocolata() {
+        return "formular_ciocolata";
+    }
+
+
+    @RequestMapping("/multumiri")
+    public String multumiri() {
+        return "multumiri";
+    }
+
+
+
+    @RequestMapping("/produse")
+    public String produse() {
+        return "produse";
+    }
+
+
+    @RequestMapping("/formular_ciocolata_personalizata")
+    public String formular_ciocolata_personalizata() {
+        return "formular_ciocolata_personalizata";
+    }
+
+
+
+
+
+
+
 }
